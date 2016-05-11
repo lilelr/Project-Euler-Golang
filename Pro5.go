e571d6c22c1bdbf8d207cfd727eba495ac1ffeff
lilelr <lilelr@163.com>
2016-05-11 20:36:11 +0800

    problem 5

---
+++ Pro5.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+	primes := []int{2,3,5,7,11,13,17,19}
+	result := 1
+	var divisor int
+	for i:=range primes{
+		divisor = primes[i]
+		for divisor <=20{
+			divisor *= primes[i]
+		}
+		divisor /=primes[i]
+		result *=divisor
+	}
+	println(result)
+}
