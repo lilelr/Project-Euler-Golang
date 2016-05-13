4d6c90643a144bad7c3da0705a6b35b718bb8f46
lilelr <lilelr@163.com>
2016-05-13 11:24:16 +0800

    problem 7

---
+++ Pro7.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+
+	arr := make([]bool, 105000)
+	arr[0], arr[1] = true, true
+	count, prime := 2, 3
+	var k int
+	for {
+		for k = 2 * prime; k < len(arr); k += prime {
+			arr[k] = true
+		}
+		for k = prime + 2; k < len(arr) && arr[k]; k += 2 {
+
+		}
+		if k < len(arr) {
+			prime = k
+			count++
+			if count == 10001 {
+				println(prime)
+				break
+			}
+		} else {
+			break
+		}
+	}
+}
